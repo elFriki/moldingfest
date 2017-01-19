93fb5778c1be125a9a06113b173c868c7ca0895c
Fernando Javier Martínez-Conde Romero <fj.martinez-con.contractor@bbva.com>
2017-01-19 08:26:07 +0100

    deploying firebase

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -6,8 +6,10 @@ export default Ember.Controller.extend({
   actions: {
     authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
+      this.get('session')
+        .authenticate('authenticator:oauth2', identification, password).catch((reason) => {
         this.set('errorMessage', reason.error || reason);
+      this.transitionTo('index');
       });
     }
   }
